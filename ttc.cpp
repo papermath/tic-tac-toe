59ab9b56407ac984a358ec88eee052fbb3129646
papermath <AndyChang943@gmail.com>
2024-08-30 09:15:19 -0500

    add introduction and include functions.hpp

--- ttc.cpp
+++ ttc.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
+#include "functions.hpp"
 
 
 // This file for main function.
 
 int main() {
+    std::cout << "Welcome to tic-tac-toe! To make a move, type in the number corresponding to the place on the grid below\n";
 
-
-  
+    std::cout << "     |     |   \n";
+    std::cout << "  7  |  8  |  9\n";
+    std::cout << "_____|_____|_____\n";
+    std::cout << "     |     |    \n";
+    std::cout << "  4  |  5  |  6\n";
+    std::cout << "_____|_____|_____\n";
+    std::cout << "     |     |   \n";
+    std::cout << "  1  |  2  |  3\n";
+    std::cout << "     |     |   \n";
+ 
 }
