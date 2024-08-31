4f75f77cdf3b0fd035aad293b0822508709e92be
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2024-08-31 11:49:54 +0100

    Update ttc.cpp

--- ttc.cpp
+++ ttc.cpp
@@ -2,10 +2,28 @@
 #include <string>
 #include "functions.hpp"
 
+#These are global variables
+
+
+
+
+
+
 
 // This file for main function.
 
 int main() {
+
+    #These are global variables
+
+    int row;
+    int column;
+    char token = 'x';
+    bool draw = false;
+    string Player1 ="";
+    string Player2="";
+    char space[3][3] = {{'1', '2', '3'},{'4', '5', '6'}, {'7', '8', '9'}};
+
     std::cout << "Welcome to tic-tac-toe! To make a move, type in the number corresponding to the place on the grid below\n";
 
     std::cout << "     |     |   \n";
