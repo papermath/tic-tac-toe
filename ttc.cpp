b51f5b276c15479dcc3d2d97194198aa046b541d
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2024-10-01 05:24:53 +0100

    Update ttc.cpp
    I worked on the main file

--- ttc.cpp
+++ ttc.cpp
@@ -1,61 +1,33 @@
-#include <iostream>
-#include <string>
+#include "functions.hpp"
 
-int main() {
-
-    //These are global variables
-
-    int row;
-    int column;
-    char token = 'x';
-    bool draw = false;
-    std::string Player1 ="";
-    std::string Player2="";
-    char space[3][3] = {{'1', '2', '3'},{'4', '5', '6'}, {'7', '8', '9'}};
-
-    std::cout << "Enter the name of Player1: \n\n";
-    std::cin >> Player1;
-    
-    std::cout << "Enter the name of Player2: \n\n";
-    std::cin >> Player2;
-
-    std::cout << Player1 << " is Player1 so he/she will play first. \n\n";
-    std::cout << Player2 << " is Player2 so he/she will start second. \n\n";
+int main(){
 
 
-    // Main code here
-    std::cout << "Welcome to tic-tac-toe! To make a move, type in the number corresponding to the place on the grid below\n";
+    std::cout << "Enter the name of the First Player here: ";
+    std::getline(std::cin, P1);                                  // This will get the input of the name of the First Player.
 
-    std::cout << "     |     |   \n";
-    std::cout << "  7  |  8  |  9\n";
-    std::cout << "_____|_____|_____\n";
-    std::cout << "     |     |    \n";
-    std::cout << "  4  |  5  |  6\n";
-    std::cout << "_____|_____|_____\n";
-    std::cout << "     |     |   \n";
-    std::cout << "  1  |  2  |  3\n";
-    std::cout << "     |     |   \n";
- 
-    while(!functionThree()){  // so here we declare your function, it could be under function one.
+    std::cout << "Enter the name of the Second Player here: ";
+    std::getline(std::cin, P2);                                 // This will get the input of the name of the Second player.
+
+    std::cout << P1 << " is Player1 so he/she will play first \n";        //This will output the following responses after Player enters his/her name.
+    std::cout << P2 << " is Player2 so he/she will play second \n";
+
+
+    while(!functionThree()){
+
         functionOne();
         functionTwo();
         functionThree();
-    }
-
-    if(token=='x' && draw == false){
-        std::cout << Player1 <<" Wins!!" << std::endl;
 
     }
 
-    else if(token=='o' && draw == false){
-        std::cout << Player2 << " Wins!!" << std::endl;
-
-    }
-
-    else{
-        std::cout  << "It is a Tie!!" << std::endl;
+ if (draw) {
+        std::cout << " IT IS A DRAW." << std::endl;
+    } else if (token == 'x') {
+        std::cout << P2 << " WINS!!!" << std::endl;
+    } else {
+        std::cout << P1 << " WINS!!!" << std::endl;
     }
 
     return 0;
-
 }
